89ca0b209134e80a3def31c04f14c96b52f809ec
BohdanMar <68649482+BohdanMar@users.noreply.github.com>
2024-05-17 11:04:22 +0200

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,21 @@
+import sys
+
+def calculate(operand1, operand2, operation):
+    if operation == '+':
+        return operand1 + operand2
+    elif operation == '-':
+        return operand1 - operand2
+    elif operation == '*':
+        return operand1 * operand2
+    elif operation == '/':
+        return operand1 / operand2
+    else:
+        raise ValueError("Invalid operation")
+
+if __name__ == "__main__":
+    operand1 = float(sys.argv[1])
+    operand2 = float(sys.argv[2])
+    operation = sys.argv[3]
+    
+    result = calculate(operand1, operand2, operation)
+    print(f"Result: {operand1} {operation} {operand2} = {result}")
